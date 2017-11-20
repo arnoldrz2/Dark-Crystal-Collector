0cf7daaaf6e5b5de8a95db2110a63e3a21d30a02
arnold <arnoldrz2@gmail.com>
2017-11-20 16:21:25 -0600

    created random target number generator function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,23 @@
 window.onload = function() {
-	
+// Random Target Number Generator
+var targetNumber = Math.floor(Math.random() * (121 - 19) + 19);
+$('#target-number').html(targetNumber);
+
+
+
+// Random Button Number Generator
+
+
+
+// Button Click Functions
+
+
+
+// Win/Lose Condition
+
+
+
+// Win/Lose Counters
 
 
 
@@ -18,7 +36,4 @@ window.onload = function() {
 
 
 
-
-
-	
 };
