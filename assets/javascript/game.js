8314b7a12794ed8a0645b662133c307f0137b1bd
arnold <arnoldrz2@gmail.com>
2017-11-21 15:24:09 -0600

    reworked random button number generator code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,13 +19,25 @@ $(document).ready(function(){
 		$('#target-number').html(targetNumber);
 
 		// Random Button Number Generator
-		// var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+		var images = ['assets/images/DC1.jpg', 'assets/images/DC2.jpg', 'assets/images/DC3.png', 'assets/images/DC4.jpg']
 
-		// for (var i = 0; i < numberOptions.length; i++) {
+		var numberOptions1 = [1, 5, 9];
+		var numberOptions2 = [2, 6, 10];
+		var numberOptions3 = [3, 7, 11];
+		var numberOptions4 = [4, 8, 12];
 
+		var number1 = numberOptions1[Math.floor(Math.random() * numberOptions1.length)];
+		var number2 = numberOptions2[Math.floor(Math.random() * numberOptions2.length)];
+		var number3 = numberOptions3[Math.floor(Math.random() * numberOptions3.length)];
+		var number4 = numberOptions4[Math.floor(Math.random() * numberOptions4.length)];
 
-
-		// }
+		for (var i = 0; i < images.length; i++) {
+			var imgCharacter = $('<img>');
+			$(imgCharacter).addClass('button');
+			$(imgCharacter).attr('src', images[i]);
+			$(imgCharacter).attr('value', number1);
+			$('#button-box').append(imgCharacter);
+		}
 
 
 		//Assign Numbers to Buttons
@@ -60,28 +72,28 @@ $(document).ready(function(){
 
 
 //Experimental Code
-		// function setNumVal1 () {
-		// 	document.getElementById("#box-1").value = box1;
-		// }
+	// function setNumVal1 () {
+	// 	document.getElementById("#box-1").value = box1;
+	// }
 
-		// function setNumVal2 () {
-		// 	document.getElementById("#box-2").value = box2;
-		// }
+	// function setNumVal2 () {
+	// 	document.getElementById("#box-2").value = box2;
+	// }
 
-		// function setNumVal3 () {
-		// 	document.getElementById("#box-3").value = box3;
-		// }
+	// function setNumVal3 () {
+	// 	document.getElementById("#box-3").value = box3;
+	// }
 
-		// function setNumVal4 () {
-		// 	document.getElementById("#box-4").value = box4;
-		// }
+	// function setNumVal4 () {
+	// 	document.getElementById("#box-4").value = box4;
+	// }
 
 
-		// var numBox1 = [1, 3, 5];
-		// var numBox2 = [2, 4, 6];
-		// var numBox3 = [7, 9, 11];
-		// var numBox4 = [8, 10, 12];
-		// var box1 = numBox1[Math.floor(Math.random() * numBox1.length)];
-		// var box2 = numBox2[Math.floor(Math.random() * numBox2.length)]; 
-		// var box3 = numBox3[Math.floor(Math.random() * numBox3.length)]; 
-		// var box4 = numBox4[Math.floor(Math.random() * numBox4.length)];
+	// var numBox1 = [1, 3, 5];
+	// var numBox2 = [2, 4, 6];
+	// var numBox3 = [7, 9, 11];
+	// var numBox4 = [8, 10, 12];
+	// var box1 = numBox1[Math.floor(Math.random() * numBox1.length)];
+	// var box2 = numBox2[Math.floor(Math.random() * numBox2.length)]; 
+	// var box3 = numBox3[Math.floor(Math.random() * numBox3.length)]; 
+	// var box4 = numBox4[Math.floor(Math.random() * numBox4.length)];
